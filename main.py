839b4703bf15f50a3a35b85df81315620d849f25
Diki Bagastama <dikibagas139@gmail.com>
2022-11-02 15:24:13 +0700

    formatted

--- main.py
+++ main.py
@@ -17,6 +17,14 @@ window_size = 7
 hidden_size = 15
 board_size = 10
 
-gen_player = GeneticPlayer(pop_size, num_generations, num_trails,
-                           window_size, hidden_size, board_size, mutation_chance=0.1, mutation_size=0.1)
+gen_player = GeneticPlayer(
+    pop_size,
+    num_generations,
+    num_trails,
+    window_size,
+    hidden_size,
+    board_size,
+    mutation_chance=0.1,
+    mutation_size=0.1,
+)
 gen_player.evolve_pop()
